9d4af386e14632a54e9dcb5b299e0e9726e4abc0
Anurag30112003 <anuragsharma011011@gmail.com>
2022-04-03 21:40:11 +0530

    feat[#3 added 404]

--- src/router/index.js
+++ src/router/index.js
@@ -21,7 +21,7 @@ const router = createRouter({
       component: () => import('../components/Projects.vue')
     },
     { 
-      path: "/:catchAll(.*)",
+      path: "*",
     name: 'NotFound',
     component: () => import('../components/NotFound.vue')
   },
