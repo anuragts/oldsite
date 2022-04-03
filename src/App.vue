4b2e567bd6fc827f97802bec474a71727c260fe7
Anurag30112003 <anuragsharma011011@gmail.com>
2022-04-04 00:48:43 +0530

    feat[contact]

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Navbar from "@/components/Navbar.vue";
+import Contact from '@/components/Contact.vue';
 
 </script>
 
 <template>
 <Navbar />
   <RouterView />
+  <Contact />
 </template>
 
 <style>
---
+++ src/components/Contact.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="contact">
+  <a href="https://twitter.com/theanuragdev" class="icon"> <i class="fa-brands fa-twitter"></i></a>
+  <a href="mailto:anuragsharma011011@gmail.com" class="icon"><i class="fa fa-envelope" aria-hidden="true"></i>
+  <a href="https://github.com/Anurag30112003" class="icon"><i class="fa-brands fa-github"></i></a>
+</a>
+</div>
+</template>
+<style scoped>
+.icon{
+    margin-left: 10px;
+    margin-right: 10px;
+}
+.icon:hover{
+    color: var(--third);
+}
+.contact {
+    margin-top: 10px;
+    text-align: center;
+    font-family: "Josefin Sans", sans-serif;
+    font-size: 2rem;
+    line-height: 1.6;
+}
+</style>
