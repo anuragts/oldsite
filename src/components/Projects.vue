70bcf0c4d82f1a02a69fe0f3f5cedca894149074
Anurag30112003 <anuragsharma011011@gmail.com>
2022-04-05 14:25:13 +0530

    feat[project template]

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,9 +1,79 @@
+<script>
+import { RouterLink, RouterView } from "vue-router";
+import MakeProject from "@/components/MakeProject.vue";
+
+export default {
+  name: "Posts",
+  components: {
+    MakeProject,
+
+    RouterLink,
+    RouterView,
+  },
+};
+</script>
 <template>
   <div class="projects">
     <div class="title">Projects</div>
+    <!-- project start -->
+    <div class="box">
+      <MakeProject
+        title="Project 1"
+        body="This is Test Project 1"
+      />
+      <div class="rt">
+        <router-link to="/" class="btn">Read-More</router-link>
+      </div>
+    </div>
+    <!-- project end -->
+     <!-- project start -->
+    <div class="box">
+      <MakeProject
+        title="Project 2"
+        body="This is Test Project 2"
+      />
+      <div class="rt">
+        <router-link to="/" class="btn">Read-More</router-link>
+      </div>
+    </div>
+    <!-- project end -->
+     <!-- project start -->
+    <div class="box">
+      <MakeProject
+        title="Project 3"
+        body="This is Test Project 3"
+      />
+      <div class="rt">
+        <router-link to="/" class="btn">Read-More</router-link>
+      </div>
+    </div>
+    <!-- project end -->
+    
   </div>
 </template>
 <style scoped>
+.btn:hover {
+  color: var(--third);
+  background-color: var(--primary);
+  border: 2px solid var(--third);
+}
+.box {
+  outline-style: solid;
+  outline-color: var(--third);
+  outline-width: 2px;
+}
+.btn {
+  background-color: var(--third);
+  color: var(--primary);
+  padding: 1rem 1.5rem;
+  text-decoration: none;
+  font-family: "Josefin Sans", sans-serif;
+  font-size: 1.3rem;
+}
+.rt {
+  text-align: center;
+  padding-bottom: 3rem;
+}
 .title {
   font-size: 40px;
   font-weight: 400;
