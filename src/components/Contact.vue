8ffe1bc9f8947abb9a1f0dba43537268b00910d2
Anurag30112003 <anuragsharma011011@gmail.com>
2022-04-06 15:17:29 +0530

    feat[Made With]

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,10 +3,15 @@
   <a href="https://twitter.com/theanuragdev" class="icon"> <i class="fa-brands fa-twitter"></i></a>
   <a href="mailto:anuragsharma011011@gmail.com" class="icon"><i class="fa fa-envelope" aria-hidden="true"></i>
   <a href="https://github.com/Anurag30112003" class="icon"><i class="fa-brands fa-github"></i></a>
+  <div class="mdw">Made with  <i class="fa-solid fa-heart" style="color: rgb(253, 91, 245);"></i> by <span class="cnt"> Anurag</span></div>
 </a>
 </div>
 </template>
 <style scoped>
+.mdw{
+    margin-top: 0.5rem;
+    font-size: 1.2rem;
+}
 .icon{
     margin-left: 10px;
     margin-right: 10px;
@@ -20,5 +25,10 @@
     font-family: "Josefin Sans", sans-serif;
     font-size: 2rem;
     line-height: 1.6;
+}
+.cnt {
+  text-decoration: underline;
+  text-decoration-color: var(--third);
+  text-decoration-thickness: 0.25rem;
 }
 </style>
